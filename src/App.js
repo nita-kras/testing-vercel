d61ed62e4749f1457b72fef8ee81f13ada514257
Jamie Vickers <psyjv3@exmail.nottingham.ac.uk>
2022-11-25 02:48:51 +0000

    Try Figure component with url from remote json

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
             .then(res => res.json())
             .then((res) => {
                 this.setState({
-                    figures: res
+                    figures: res["in"]
                 })
             })
     }
@@ -41,11 +41,11 @@ class App extends React.Component {
 
     render() {
         document.title = "Figure Viewer";
-        console.log(this.state.figures["in"])
         return (
             <div className="App">
                 <button className="buttons" onClick={() => this.changeFigure(false)}>Previous</button>
                 <div className="Figure">
+                    <Figure imgUrl={this.state.figures[0]["url"]}/>
                     <p id={"figure-label"}>Figure {this.state.currentFigureIndex + 1}</p>
                 </div>
                 <button className="buttons" onClick={() => this.changeFigure(true)}>Next</button>
